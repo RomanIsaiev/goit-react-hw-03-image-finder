7fdccffa7d9fc6a0dd8993bf9243e3b8fec0a8b5
Roman Isaiev <roma.isaev.2000@gmail.com>
2023-11-09 00:24:22 +0200

    searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import axios from 'axios';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {
+    searchQuery: '',
+    // loading: false,
+  };
+
+  handleFormSubmit = searchQuery => {
+    console.log(searchQuery);
+    this.setState({ searchQuery });
+  };
+
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   axios({
+  //     method: 'get',
+  //     url: `https://pixabay.com/api/?q=cat&page=1&key=39745378-dd24554a2bcf90950765bc548&image_type=photo&orientation=horizontal&per_page=12`,
+  //   })
+  //     .then(response => response.data)
+  //     .then(apiArray => apiArray.hits)
+  //     .then(oneImage => oneImage[0])
+  //     .then(images => {
+  //       console.log(images);
+  //       return this.setState({ images });
+  //     })
+  //     .finally(() => this.setState({ loading: false }));
+  // }
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {/* {this.state.loading && <h2>Загрузка...</h2>}
+        {this.state.images && <div>{this.state.images.previewURL}</div>} */}
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,48 @@
+import { Component } from 'react';
+import { BiSearchAlt2 } from 'react-icons/bi';
+
+export class Searchbar extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  handleSearchQueryChange = event => {
+    this.setState({ searchQuery: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSearchQuerySubmit = event => {
+    event.preventDefault();
+
+    if (this.state.searchQuery.trim() === '') {
+      alert('The field must not be empty');
+      return;
+    }
+
+    this.props.onSubmit(this.state.searchQuery);
+    this.setState({ searchQuery: '' });
+  };
+
+  render() {
+    return (
+      <header className="searchbar">
+        <form className="form" onSubmit={this.handleSearchQuerySubmit}>
+          <input
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleSearchQueryChange}
+          />
+
+          <button type="submit" className="button">
+            <span className="button-label">
+              <BiSearchAlt2 style={{ marginRight: 8 }} />
+              Search
+            </span>
+          </button>
+        </form>
+      </header>
+    );
+  }
+}
